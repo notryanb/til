cac093d329aa0c94224303cc7b3a3e0eb82afe5a
Ryan B <notryanb@gmail.com>
2017-02-28 10:01:00 -0500

    Update User & Post models to support relation. Add Bcrypt on User model

--- api/src/models/post.js
+++ api/src/models/post.js
@@ -1,14 +1,45 @@
 'use strict';
+
 module.exports = function(sequelize, DataTypes) {
-  var post = sequelize.define('post', {
-    title: DataTypes.TEXT,
-    body: DataTypes.TEXT,
-    published_date: DataTypes.DATE,
-    published: DataTypes.BOOLEAN
+  var post = sequelize.define('Post', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.TEXT,
+      validate: {
+        notEmpty: true,
+        len: [1, 100]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      validate: {
+        notEmpty: true
+      }
+    },
+    published_date: {
+      type: DataTypes.DATE
+    },
+    published: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false;
+    }
   }, {
+    underscored: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    tableName: 'posts',
     classMethods: {
       associate: function(models) {
-        post.belongsTo(models.user, { foreignKey: 'user_id' });
+        post.belongsTo(models.User, { foreignKey: 'user_id' });
+      }
+    },
+    getterMethods : {
+      createdAt: function() {
+        return this.created_at;
       }
     }
   });
